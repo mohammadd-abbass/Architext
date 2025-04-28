6032299aa5451d06c261131c43c876395d32f7c6
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-29 01:11:59 +0300

    Added safity check if the editor was not recognized

--- vscode-extension/src/commands/commentFunction.ts
+++ vscode-extension/src/commands/commentFunction.ts
@@ -2,5 +2,10 @@ import * as vscode from 'vscode';
 import { callFlaskAPI } from '../services/apiClient';
 
 export async function commentFunction() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+  
 
 }
