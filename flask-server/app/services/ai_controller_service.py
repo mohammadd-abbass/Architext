5b55f59737b8c9eee5368eb11c6583b0b649973a
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-28 23:41:00 +0300

    Implemented ai controller service

---
+++ flask-server/app/services/ai_controller_service.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, jsonify
+from models.classes import generateCommentsRequestModel
+
+ai = Blueprint("ai", __name__)
+
+@ai.route("/generateComments", methods=["POST"])
+def generate_comments(body: generateCommentsRequestModel):
+    code = body.code
+    language = body.language
+
+    if not code:
+        return jsonify({ "error": "No code provided"}), 400
+    
+    result = generate_function_comments(code, language)
+    return jsonify({ "code": result })
