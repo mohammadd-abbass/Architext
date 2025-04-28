3adf3edbda318b4f038ecb16cbe29c3f8bb552ea
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-28 23:33:03 +0300

    Implemented flask app.py server

--- flask-server/app/app.py
+++ flask-server/app/app.py
@@ -1,1 +1,11 @@
-print("Testing flask")
+from flask import Flask
+from flask_cors import CORS
+from controllers.ai_controller import ai
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(ai, url_prefix = "/ai")
+
+if __name__ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
