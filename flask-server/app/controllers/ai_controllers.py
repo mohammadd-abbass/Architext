c6fc1a0e92911746ae66dd10754aa60830a91774
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-29 22:05:50 +0300

    Implemented calculate complexity controller

--- flask-server/app/controllers/ai_controllers.py
+++ flask-server/app/controllers/ai_controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
-from models.classes import generateCommentsRequestModel
-from services.ai_controller_service import generate_function_comments
+from models.classes import generateCommentsRequestModel, calculateComplexityRequestModel
+from services.ai_controller_service import generate_function_comments, calculate_function_complexity
 from pydantic import ValidationError
 
 ai = Blueprint("ai", __name__)
@@ -21,6 +21,26 @@ def generate_comments():
     
     try:
         result = generate_function_comments(code, language)
+        return jsonify({"code": result})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@ai.route("/calculateComplexity", methods=["POST"])
+def calculate_complexity():
+    try:
+        body = calculateComplexityRequestModel(**request.get_json())
+    except ValidationError as e:
+        print(e)
+        return jsonify({"error": str(e)}), 400
+
+    code = body.code
+    language = body.language
+
+    if not code:
+        return jsonify({"error": "No code provided"}), 400
+    
+    try:
+        result = calculate_function_complexity(code, language)
         return jsonify({"code": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
