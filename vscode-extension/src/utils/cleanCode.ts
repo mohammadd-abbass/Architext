75c7312a86085c7cf1eab983439b1f97c095f5a0
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-29 13:51:11 +0300

    Created clean code utility for safety response

---
+++ vscode-extension/src/utils/cleanCode.ts
@@ -0,0 +1,10 @@
+function cleanCodeResponse(rawCode: string) {
+    if (rawCode.startsWith("```")) {
+        rawCode = rawCode.split('\n').slice(1).join('\n');
+
+        if (rawCode.endsWith('```')) {
+            rawCode = rawCode.slice(0, -3).trim();
+        }
+    }
+    return rawCode.trim();
+}
