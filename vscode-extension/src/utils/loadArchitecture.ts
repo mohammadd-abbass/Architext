b4cfdc89a3817f01e1b54ccfa82f73892868b9cc
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-01 15:00:22 +0300

    Refactored load architecture file for more readability

--- vscode-extension/src/utils/loadArchitecture.ts
+++ vscode-extension/src/utils/loadArchitecture.ts
@@ -1,15 +1,18 @@
 import * as vscode from 'vscode';
 
-
-export const loadArchitecture = async () => {
+export const loadArchitecture = async (): Promise<any> => {
     const files = await vscode.workspace.findFiles('*.arch.json');
-    if (files.length) {
-      const content = await vscode.workspace.fs.readFile(files[0]);
-      return JSON.parse(content.toString());
-    } else {
-      const defaultArch = require('../default.arch.json');
-      return defaultArch;
+
+    try {
+        if (files.length > 0) {
+            const content = await vscode.workspace.fs.readFile(files[0]);
+            return JSON.parse(content.toString());
+        } else {
+            const defaultArch = require('../default.arch.json');
+            return defaultArch;
+        }
+    } catch (error) {
+        vscode.window.showErrorMessage('Failed to load architecture file.');
+        return null;
     }
 };
-
-
