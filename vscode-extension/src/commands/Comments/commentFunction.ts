de44b6db85586590c093beab0c4577b7e4b8528a
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-02 16:17:28 +0300

    Updated the comment-function function to use the  editor util

--- vscode-extension/src/commands/Comments/commentFunction.ts
+++ vscode-extension/src/commands/Comments/commentFunction.ts
@@ -1,35 +1,12 @@
-import * as vscode from 'vscode';
 import { commentCodeAPI } from '../../services/commentAPI';
+import { processEditorAction } from '../../utils/editorUtils';
+
 
 export const commentFunction = async () => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        return;
-    } 
-
-    const selection = editor.selection;
-    const selectedText = editor.document.getText(selection);
-    const language = editor.document.languageId;
-
-    try {
-        vscode.window.withProgress({
-        location: vscode.ProgressLocation.Notification,
-        title: "Generating comments..."
-        }, async () => {
-
-            const result = await commentCodeAPI(selectedText, language); 
-        
-            const commentedCode = result.code;
-        
-            editor.edit(editBuilder => {
-            editBuilder.replace(selection, commentedCode);
-            });
-        
-            vscode.window.showInformationMessage('Comments added to selected code!');
-        });
-
-
-    }catch(error : any){
-        vscode.window.showErrorMessage(`Failed to add comments: ${error.message}`);
-    }
+    await processEditorAction({
+        title: "Generating comments...",
+        useSelection: true,
+        apiCall: commentCodeAPI,
+        successMessage: "Comments added to selected code!"
+    });
 };
