c3750c959e9f5d3f381efe44f37e3985c7fa96d9
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-02 13:32:04 +0300

    Removed old commands from the extension.ts

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,27 +1,13 @@
 import * as vscode from 'vscode';
-import { commentCode } from './commands/commentCode';
-import { commentFunction } from './commands/commentFunction';
-import { calculateComplexityCode } from './commands/calcComplexity';
-import { calculateFunctionComplexity } from './commands/funcComplexity';
-import { checkArchitecture } from './commands/checkArchitecture';
+import { registerCommands } from './commands';
+
 
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Architext is Active!');
 
-	
-	const codeComments = vscode.commands.registerCommand('architext.addCodeComments', commentCode);
-
-	const functionComment = vscode.commands.registerCommand('architext.commentSelectedCode', commentFunction);
-
-	const codeComplexity = vscode.commands.registerCommand('architext.caculateCodeComplexity', calculateComplexityCode);
-
-	const functionComplexity = vscode.commands.registerCommand('architext.complexitySelectedCode', calculateFunctionComplexity);
-
-	const checkProjectArchitecture = vscode.commands.registerCommand('architext.checkArchitecture', () => checkArchitecture(context));
-
-	context.subscriptions.push(codeComments, functionComment, codeComplexity, functionComplexity, checkProjectArchitecture);
+	registerCommands(context);
 }
 
 export function deactivate() {}
