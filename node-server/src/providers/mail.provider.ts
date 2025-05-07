bda3a71d55c20eca4c9c6e78baf32d666dbc68a3
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-08 00:50:18 +0300

    Added commented version of send welcome email =

--- node-server/src/providers/mail.provider.ts
+++ node-server/src/providers/mail.provider.ts
@@ -16,3 +16,30 @@ export const sendWelcomeEmail = async (to: string, name: string) => {
     html: `<h3>Hello ${name}!</h3><p>Welcome to our service. We're glad to have you.</p>`,
   });
 };
+
+
+// export const sendWelcomeEmail = async (to: string, name: string) => {
+//     const transporter = nodemailer.createTransport({
+//       service: 'gmail',
+//       auth: {
+//         type: 'OAuth2',
+//         user: process.env.SMTP_EMAIL,
+//         clientId: process.env.OAUTH_CLIENTID,
+//         clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+//         accessToken: process.env.OAUTH_ACCESS_TOKEN,
+//       },
+//     });
+  
+//     try {
+//       await transporter.sendMail({
+//         from: `"Architext" <${process.env.SMTP_EMAIL}>`,
+//         to,
+//         subject: 'Welcome to Architext',
+//         html: `<h3>Hello ${name}!</h3><p>Welcome to our service. We're glad to have you.</p>`,
+//       });
+//     } catch (error) {
+//       console.error('Email sending error:', error);
+//       throw new Error('Failed to send welcome email');
+//     }
+//   };
