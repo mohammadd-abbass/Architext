0fc576e034c732d4244e15e544b25438587323df
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-08 01:21:11 +0300

    Added validator middleware to the auth routes

--- node-server/src/app/middlewares/authValidator.middleware.ts
+++ node-server/src/app/middlewares/authValidator.middleware.ts
@@ -4,7 +4,7 @@ import { validationResult } from 'express-validator';
 export const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    res.status(400).json({ errors: errors.array() });
   }
   next();
 };
--- node-server/src/routes/api/auth.routes.ts
+++ node-server/src/routes/api/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as auth from '../../app/controllers/auth.controller.js';
+import { validate } from '../../app/middlewares/authValidator.middleware.js';
+import { loginValidator, registerValidator } from '../../app/validators/auth.validator.js'; // adjust path if needed
 
 const router = Router();
 
-router.post('/signup', auth.signupHandler);
-router.post('/login', auth.loginHandler);
+router.post('/signup', registerValidator, validate, auth.signupHandler);
+router.post('/login', loginValidator, validate, auth.loginHandler);
 
 export default router;
