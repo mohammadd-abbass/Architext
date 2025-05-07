77574688648cd1f82ea7ed7add416c415870c0db
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-08 00:34:49 +0300

    Updated the auth routes to use the correct controllers

--- node-server/src/routes/api/auth.routes.ts
+++ node-server/src/routes/api/auth.routes.ts
@@ -3,7 +3,7 @@ import * as auth from '../../app/controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/signup', auth.loginHandler);
-router.post('/login', auth.signupHandler);
+router.post('/signup', auth.signupHandler);
+router.post('/login', auth.loginHandler);
 
 export default router;
