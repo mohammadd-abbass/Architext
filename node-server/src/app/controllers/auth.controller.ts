a4a40dbeccb2b907a6c81fde31053e7241b541e2
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-07 22:18:49 +0300

    Removed return from the auth controller

--- node-server/src/app/controllers/auth.controller.ts
+++ node-server/src/app/controllers/auth.controller.ts
@@ -4,18 +4,18 @@ import { successResponse, errorResponse } from '../traits/response.trait';
 
 export const signupHandler = async (req: Request, res: Response) => {
     try {
-    const user = await authService.signup(req.body);
-    return successResponse(res, 'User created successfully', user, 201);
+        const user = await authService.signup(req.body);
+        successResponse(res, 'User created successfully', user, 201);
     } catch (err: any) {
-    return errorResponse(res, err.message, 400);
+        errorResponse(res, err.message, 400);
     }
 };
 
 export const loginHandler = async (req: Request, res: Response) => {
     try {
         const { token, user } = await authService.login(req.body);
-        return successResponse(res, 'Login successful', { user, token });
+        successResponse(res, 'Login successful', { user, token });
     } catch (err: any) {
-        return errorResponse(res, err.message, 401);
+        errorResponse(res, err.message, 401);
     }
 };
