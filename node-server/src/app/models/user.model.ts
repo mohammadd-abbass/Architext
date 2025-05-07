4a98e813b8484fb9af9a0cf7815865918d3f2f7e
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-08 00:34:05 +0300

    Edited the schema.prisma generator client output path

--- node-server/src/app/models/user.model.ts
+++ node-server/src/app/models/user.model.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../../config/db.js';
 
 export const createUser = async (data: { name: string; email: string; password: string }) => {
   return prisma.user.create({ data });
--- node-server/src/main.ts
+++ node-server/src/main.ts
@@ -1,10 +1,17 @@
 
 import dotenv from 'dotenv';
 import app from './bootstrap/server.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
