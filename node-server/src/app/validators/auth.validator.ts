d7e1d8fbd9bfa546a67f338f74311cfa18daa3df
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-08 01:09:06 +0300

    Implemented login validator

--- node-server/src/app/validators/auth.validator.ts
+++ node-server/src/app/validators/auth.validator.ts
@@ -5,3 +5,8 @@ export const registerValidator = [
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
   body('name').notEmpty().withMessage('name is required'),
 ];
+
+export const loginValidator = [
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('password').notEmpty().withMessage('Password is required'),
+];
