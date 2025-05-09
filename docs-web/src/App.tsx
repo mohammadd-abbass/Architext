e6ba04a72d7c3ec22aa0d911fc58170c98396312
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-09 22:47:12 +0300

    Tailwind initial config

--- docs-web/src/App.tsx
+++ docs-web/src/App.tsx
@@ -1,9 +1,13 @@
-
+import './assets/style/globals.css'
 function App() {
 
   return (
     <>
-    
+      <h1>Vite + React + TypeScript + Tailwind CSS</h1>
+      <p>Click the button below to see the magic happen!</p>
+      <button className="text-white font-bold py-2 px-4 rounded bg-blue-700">
+        Click me!
+      </button>
     </>
   )
 }
