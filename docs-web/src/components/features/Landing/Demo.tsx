fcbe0e83b8bc553b2d9098a08fb5282612b04079
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 02:14:05 +0300

    Implemented initial demo section

--- docs-web/src/components/features/Landing/Demo.tsx
+++ docs-web/src/components/features/Landing/Demo.tsx
@@ -1,11 +1,23 @@
 import SectionWrapper from '../../common/SectionWrapper';
 import SectionTitle from '../../common/SectionTitle';
+import demo from '../../../assets/images/demo.png'
 
 const DemoSection = () => {
   return (
     <SectionWrapper>
-      <SectionTitle>Demo</SectionTitle>
-      {/* Demo content */}
+      <SectionTitle>See It in Action</SectionTitle>
+      
+      <div className="max-w-4xl mx-auto">
+
+          <div className="rounded-lg overflow-hidden border-1 border-secondary/20 ">
+            {/* Placeholder image - replace with GIF later */}
+            <img
+              src={demo}
+              alt="Architext extension demo"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+      </div>
     </SectionWrapper>
   );
 };
