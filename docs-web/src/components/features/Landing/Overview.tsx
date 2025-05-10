cd4abc9c7394dfda31c9c35c1f66157fafe4b9c4
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 01:16:56 +0300

    Implemented overview section empty component

---
+++ docs-web/src/components/features/Landing/Overview.tsx
@@ -0,0 +1,14 @@
+// components/sections/OverviewSection.tsx
+import SectionWrapper from '../../common/SectionWrapper';
+import SectionTitle from '../../common/SectionTitle';
+
+const OverviewSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Overview</SectionTitle>
+      {/* Overview content */}
+    </SectionWrapper>
+  );
+};
+
+export default OverviewSection;
