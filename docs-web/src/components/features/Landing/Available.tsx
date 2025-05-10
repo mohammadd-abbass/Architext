3616134941e9b94512cdcd5f68f9b9aea2137b42
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 01:20:38 +0300

    Created sections empty components

---
+++ docs-web/src/components/features/Landing/Available.tsx
@@ -0,0 +1,13 @@
+import SectionWrapper from '../../common/SectionWrapper';
+import SectionTitle from '../../common/SectionTitle';
+
+const AvailableSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Currently on</SectionTitle>
+      {/* Available content */}
+    </SectionWrapper>
+  );
+};
+
+export default AvailableSection;
---
+++ docs-web/src/components/features/Landing/Features.tsx
@@ -0,0 +1,14 @@
+// components/sections/OverviewSection.tsx
+import SectionWrapper from '../../common/SectionWrapper';
+import SectionTitle from '../../common/SectionTitle';
+
+const FeaturesSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Features</SectionTitle>
+      {/* Features content */}
+    </SectionWrapper>
+  );
+};
+
+export default FeaturesSection;
