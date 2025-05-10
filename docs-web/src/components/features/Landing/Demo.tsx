91ff3747efe08e89a6ab132fd357452601ab6dd2
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 01:20:38 +0300

    Created sections empty components

---
+++ docs-web/src/components/features/Landing/Demo.tsx
@@ -0,0 +1,13 @@
+import SectionWrapper from '../../common/SectionWrapper';
+import SectionTitle from '../../common/SectionTitle';
+
+const DemoSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Demo</SectionTitle>
+      {/* Demo content */}
+    </SectionWrapper>
+  );
+};
+
+export default DemoSection;
---
+++ docs-web/src/components/features/Landing/Faqs.tsx
@@ -0,0 +1,14 @@
+// components/sections/OverviewSection.tsx
+import SectionWrapper from '../../common/SectionWrapper';
+import SectionTitle from '../../common/SectionTitle';
+
+const FaqsSection = () => {
+  return (
+    <SectionWrapper>
+      <SectionTitle>Faqs</SectionTitle>
+      {/* Faqs content */}
+    </SectionWrapper>
+  );
+};
+
+export default FaqsSection;
