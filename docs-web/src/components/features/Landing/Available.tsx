6382b809c3d5638ec940a44d3b1aa838ce6047b1
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 01:51:30 +0300

    Implemented available on section

--- docs-web/src/components/features/Landing/Available.tsx
+++ docs-web/src/components/features/Landing/Available.tsx
@@ -1,11 +1,24 @@
 import SectionWrapper from '../../common/SectionWrapper';
 import SectionTitle from '../../common/SectionTitle';
+import Container from '../../common/Container';
+import Vscode from '../../../assets/images/vs-code.svg';
+
 
 const AvailableSection = () => {
   return (
     <SectionWrapper>
       <SectionTitle>Currently on</SectionTitle>
-      {/* Available content */}
+      <div className="flex flex-col items-center justify-center">
+        <Container className="max-w-30 text-center">
+          <div className="flex flex-col items-center">
+            <img src={Vscode} alt="" />
+          </div>
+        </Container>
+        <p className="text-secondary/80 text-lg text-center my-10">
+            Architext is currently available as a Visual Studio Code extension.<br />
+            <span className="text-accent/80 text-center">Support for other editors coming soon.</span>
+        </p>
+      </div>
     </SectionWrapper>
   );
 };
