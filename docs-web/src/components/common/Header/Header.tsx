7a52603d9d6c1459b11192f3935ff4624940c8cf
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 01:10:27 +0300

    Created section title file

--- docs-web/src/components/common/Header/Header.tsx
+++ docs-web/src/components/common/Header/Header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="bg-primary py-2 flex items-center justify-center relative">
+    <header className="bg-primary py-5 flex items-center justify-center relative">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Left Section */}
