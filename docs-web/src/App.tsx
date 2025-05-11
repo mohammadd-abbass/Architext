b92969d175026dfd1b219a2232a8bf87780f5c8b
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-11 20:45:49 +0300

    Implemented docs sidebar

--- docs-web/src/App.tsx
+++ docs-web/src/App.tsx
@@ -1,6 +1,6 @@
 import './assets/style/globals.css'
-// import DocsLayout from './layouts/DocsLayout'
-import LandingPage from './pages/LandingPage'
+import DocsLayout from './layouts/DocsLayout'
+// import LandingPage from './pages/LandingPage'
 // import MainLayout from './layouts/MainLayout'
 // import LandingPage from './pages/LandingPage'
 // import { AuthPage } from './pages/AuthPage'
@@ -9,7 +9,17 @@ function App() {
 
   return (
     <>
-      <LandingPage/>
+      <DocsLayout>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+      </DocsLayout>
     </>
   ) 
 }
