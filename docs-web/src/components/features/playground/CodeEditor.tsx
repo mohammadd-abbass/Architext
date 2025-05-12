ba1a98b0ea3ba33f846a5d01b0fb0433627a43bb
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-12 17:42:39 +0300

    Impelemtend monaco editor component

---
+++ docs-web/src/components/features/playground/CodeEditor.tsx
@@ -0,0 +1,32 @@
+import Editor from '@monaco-editor/react';
+
+interface CodeEditorProps {
+  code: string;
+  onChange: (value: string) => void;
+}
+
+export default function CodeEditor({ code, onChange }: CodeEditorProps) {
+  return (
+    <Editor
+      height="100%"
+      defaultLanguage="typescript"
+      defaultValue={code}
+      onChange={(value) => onChange(value || '')}
+      theme="vs-dark"
+      options={{
+        minimap: { enabled: true },
+        fontSize: 14,
+        lineNumbers: 'on',
+        roundedSelection: false,
+        scrollBeyondLastLine: false,
+        readOnly: false,
+        automaticLayout: true,
+        scrollbar: {
+          verticalSliderSize: 6,
+          horizontalSliderSize: 6,
+        },
+        glyphMargin: true,
+      }}
+    />
+  );
+}
