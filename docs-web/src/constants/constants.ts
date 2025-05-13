86159e8c25dabf5ca925363239e898e40c4cdd2c
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-14 01:39:33 +0300

    Implemented constants file for the github config links

---
+++ docs-web/src/constants/constants.ts
@@ -0,0 +1,10 @@
+export const GITHUB_CONFIG = {
+    USERNAME: "mohammadd-abbass",
+    REPO_NAME: "Architext",
+    LINKS: {
+      REPO: "https://github.com/mohammadd-abbass/Architext",
+      STAR: "https://github.com/mohammadd-abbass/Architext/stargazers",
+      FORK: "https://github.com/mohammadd-abbass/Architext/fork",
+      ISSUES: "https://github.com/mohammadd-abbass/Architext/issues",
+    },
+  };
