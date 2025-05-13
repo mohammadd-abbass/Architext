f584bc45c5b5ac7af2872b1f2858bbcb174f8479
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-13 17:33:21 +0300

    Updated the user model

--- node-server/src/app/models/user.model.ts
+++ node-server/src/app/models/user.model.ts
@@ -1,10 +1,30 @@
 import { prisma } from '../../config/db.js';
 
-export const createUser = async (data: { name: string; email: string; password: string }) => {
-  return prisma.user.create({ data });
+export const createUser = async (data: { 
+  name: string; 
+  email: string; 
+  password: string 
+}) => {
+  return prisma.user.create({
+    data,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+    }
+  });
 };
 
-
 export const findUserByEmail = async (email: string) => {
-    return prisma.user.findUnique({ where: { email } });
+  return prisma.user.findUnique({ 
+    where: { email },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true, 
+      createdAt: true
+    }
+  });
 };
