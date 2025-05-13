6986471b9af743a0878385e375532b4012717de2
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-13 23:01:19 +0300

    Implemented shell.sh file commands

---
+++ node-server/shell.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Installing dependencies..."
+npm install
+
+echo "Running Prisma migrations..."
+npx prisma migrate deploy
+
+echo "Running Prisma Sedder..."
+npx prisma generate
+# npx prisma db seed
+
+echo "Starting server..."
+npm run start:prod
