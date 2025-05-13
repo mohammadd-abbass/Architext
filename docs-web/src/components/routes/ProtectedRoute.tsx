731ae1b6536b8a5ccfb8ef6180d6f1d725124be4
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-13 20:46:49 +0300

    Implemented protected route

---
+++ docs-web/src/components/routes/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import authService from '../../features/auth/authService';
+import Loader from '../common/Loader';
+
+const ProtectedRoute = () => {
+  const [loading, setLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const isValid = await authService.validateToken();
+      setIsAuth(isValid);
+      if (!isValid) localStorage.clear();
+      setLoading(false);
+    };
+
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setIsAuth(false);
+      setLoading(false);
+    } else {
+      checkToken();
+    }
+  }, []);
+
+  if (loading) return <Loader />;
+  return isAuth ? <Outlet /> : <Navigate to="/auth" replace />;
+};
+
+export default ProtectedRoute;
