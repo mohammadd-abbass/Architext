b1255700f21d41831d533b6b9ce3e7298e1d9500
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-13 16:41:41 +0300

    Implemented store.ts file

--- docs-web/src/pages/AuthPage.tsx
+++ docs-web/src/pages/AuthPage.tsx
@@ -1,29 +1,10 @@
-// pages/AuthPage.tsx
 import {  useState } from 'react';
 import { AuthForm } from '../components/features/auth/AuthForm';
 // import { useNavigate } from 'react-router-dom';
-// import useAuth from './useAuth';
+import useAuth from '../hooks/useAuth';
 
 export const AuthPage = () => {
-//   const { user, login, signUp, isLoading, error, token } = useAuth();
   const [isLoginForm, setIsLoginForm] = useState(true);
-//   const navigate = useNavigate();
-
-//   const handleAuthSubmit = (formData: { email: string; password: string; name?: string }) => {
-//     if (isLoginForm) {
-//       login(formData.email, formData.password);
-//     } else {
-//       if (formData.name) {
-//         signUp(formData.name, formData.email, formData.password);
-//       }
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (user && token) {
-//       navigate('/home');
-//     }
-//   }, [user, token, navigate]);
 
   return (
     <div className="auth-page flex flex-col justify-center items-center min-h-screen relative px-6 md:px-6">
@@ -31,10 +12,8 @@ export const AuthPage = () => {
         <AuthForm
             isLogin={isLoginForm}
             toggleAuthMode={() => setIsLoginForm((prev) => !prev)}
-            // onSubmit={(handleAuthSubmit)}
             onSubmit={()=>{}}
-            // error={error}
-            // isLoading={isLoading}
+
         />
     </div>
   );
