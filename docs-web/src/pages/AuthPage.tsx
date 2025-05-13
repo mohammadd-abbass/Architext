aad5473b816b180375ae0f57ec5bf9e886865dd0
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-13 18:53:27 +0300

    Added loader for the login page

--- docs-web/src/pages/AuthPage.tsx
+++ docs-web/src/pages/AuthPage.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthForm } from '../components/features/auth/AuthForm';
 import useAuth from '../hooks/useAuth';
+import Loader from '../components/common/Loader';
 
 export const AuthPage = () => {
   const [isLoginForm, setIsLoginForm] = useState(true);
@@ -25,6 +26,7 @@ export const AuthPage = () => {
   }, [user, navigate]);
 
   return (
+    loading? <Loader/> :
     <div className="auth-page flex flex-col justify-center items-center min-h-screen relative px-6 md:px-6">
       <h1 className="title absolute left-8 top-8 text-4xl font-bold text-accent">Architext</h1>
       <AuthForm
