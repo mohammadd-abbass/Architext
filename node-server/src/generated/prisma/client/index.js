8fb6e35e03596e218c52b00beec57d85e8e5e464
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-14 18:35:55 +0300

    Implemeted create Architecture session function in the ai model

--- node-server/src/generated/prisma/client/index.js
+++ node-server/src/generated/prisma/client/index.js
@@ -202,11 +202,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgresdb123@localhost:5432/Architext?schema=public"
+        "value": null
       }
     }
   },
