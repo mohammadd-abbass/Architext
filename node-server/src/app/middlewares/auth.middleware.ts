549a4b7f203f487d8f9acb1b4b615cd86ea47298
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-14 22:06:40 +0300

    Optimized auth middleware

--- node-server/src/app/middlewares/auth.middleware.ts
+++ node-server/src/app/middlewares/auth.middleware.ts
@@ -1,12 +1,20 @@
-import passport from '../../providers/passport.provider';
 import { Request, Response, NextFunction } from 'express';
+import passport from '../../providers/passport.provider';
 
-export const authenticateJwt = (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('jwt', { session: false }, (err: Error, user: any, info: any) => {
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  passport.authenticate('jwt', { session: false }, (err: Error, user: any) => {
     if (err) return next(err);
-    if (!user) return res.status(401).json({ message: 'Unauthorized' });
-
-    req.user = user; 
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+    
+    req.user = {
+      id: user.id,
+      email: user.email,
+      name: user.name
+    };
     next();
-  })(req, res, next); 
+  })(req, res, next);
 };
