42eb3d8f2886a99aacab6788cd6d277cf7c1c3fd
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 11:42:27 +0300

    Added assistant reducer to the store

--- docs-web/src/store/store.ts
+++ docs-web/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import assistantReducer from '../features/assistant/assistantSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    assistant: assistantReducer,
 
   },
   middleware: (getDefaultMiddleware) => 
