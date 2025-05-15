74c4865df3e04fe60d86e3e3a1f130c61a900d0e
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 17:38:25 +0300

    Added the logs service to the auth service

--- node-server/src/app/controllers/auth.controller.ts
+++ node-server/src/app/controllers/auth.controller.ts
@@ -5,8 +5,8 @@ import jwt from 'jsonwebtoken';
 
 export const signupHandler = async (req: Request, res: Response) => {
     try {
-        const user = await authService.signup(req.body);
-        successResponse(res, 'User created successfully', user, 201);
+      const user = await authService.signup({ ...req.body, req });
+      successResponse(res, 'User created successfully', user, 201);
     } catch (err: any) {
         errorResponse(res, err.message, 400);
     }
@@ -14,11 +14,11 @@ export const signupHandler = async (req: Request, res: Response) => {
 
 export const loginHandler = async (req: Request, res: Response) => {
     try {
-        const { token, user } = await authService.login(req.body);
+        const { token, user } = await authService.login({ ...req.body, req });
         successResponse(res, 'Login successful', { user, token });
     } catch (err: any) {
         errorResponse(res, err.message, 401);
-    }
+      }
 };
 
 export const validateTokenHandler = async (req: Request, res: Response) => {
