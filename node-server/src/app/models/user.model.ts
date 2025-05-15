a7aa470fd555c9157e33f9e33dc260b9aaf6f01d
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 20:13:19 +0300

    Implemented find or create github user model function

--- node-server/src/app/models/user.model.ts
+++ node-server/src/app/models/user.model.ts
@@ -1,9 +1,9 @@
-import { prisma } from '../../config/db.js';
+import { prisma } from "../../config/db.js";
 
-export const createUser = async (data: { 
-  name: string; 
-  email: string; 
-  password: string 
+export const createUser = async (data: {
+  name: string;
+  email: string;
+  password: string;
 }) => {
   return prisma.user.create({
     data,
@@ -12,19 +12,37 @@ export const createUser = async (data: {
       name: true,
       email: true,
       createdAt: true,
-    }
+    },
   });
 };
 
 export const findUserByEmail = async (email: string) => {
-  return prisma.user.findUnique({ 
+  return prisma.user.findUnique({
     where: { email },
     select: {
       id: true,
       name: true,
       email: true,
-      password: true, 
-      createdAt: true
-    }
+      password: true,
+      createdAt: true,
+    },
+  });
+};
+
+export const findOrCreateGitHubUser = async (profile: any) => {
+  const existingUser = await prisma.user.findUnique({
+    where: { githubId: profile.id.toString() },
+  });
+
+  if (existingUser) return existingUser;
+
+  return prisma.user.create({
+    data: {
+      githubId: profile.id.toString(),
+      name: profile.displayName || profile.username,
+      email: profile.emails?.[0]?.value,
+      password: "",
+    },
+    select: { id: true, email: true, name: true, createdAt: true },
   });
 };
