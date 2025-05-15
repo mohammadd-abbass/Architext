3d77d08e29c12abf22e7e4fbc974608f240953f5
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 17:57:54 +0300

    Updated the auth controller to use the log service

--- node-server/src/app/controllers/auth.controller.ts
+++ node-server/src/app/controllers/auth.controller.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import * as authService from '../services/auth.service.js';
 import { successResponse, errorResponse } from '../traits/response.trait.js';
 import jwt from 'jsonwebtoken';
+import { createLog } from '../services/log.service.js';
 
 export const signupHandler = async (req: Request, res: Response) => {
     try {
-      const user = await authService.signup({ ...req.body, req });
+      const user = await authService.signup(req.body);
+
+      await createLog(user.user.id, req.ip  || 'unknown', req.headers["user-agent"]);
+
       successResponse(res, 'User created successfully', user, 201);
     } catch (err: any) {
         errorResponse(res, err.message, 400);
@@ -14,7 +18,10 @@ export const signupHandler = async (req: Request, res: Response) => {
 
 export const loginHandler = async (req: Request, res: Response) => {
     try {
-        const { token, user } = await authService.login({ ...req.body, req });
+        const { token, user } = await authService.login(req.body);
+
+        await createLog(user.id, req.ip || 'unknown', req.headers["user-agent"]);
+
         successResponse(res, 'Login successful', { user, token });
     } catch (err: any) {
         errorResponse(res, err.message, 401);
