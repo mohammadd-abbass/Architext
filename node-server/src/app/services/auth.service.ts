efc502226b0ca1bede7645ca828fff7951b3c04e
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 17:38:25 +0300

    Added the logs service to the auth service

--- node-server/src/app/services/auth.service.ts
+++ node-server/src/app/services/auth.service.ts
@@ -2,10 +2,11 @@ import jwt from "jsonwebtoken";
 import { createUser, findUserByEmail } from "../models/user.model.js";
 import { sendWelcomeEmail } from "../../providers/mail.provider.js";
 import bcrypt from "bcrypt";
+import { createLog } from "./log.service.js";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 
-export const signup = async ({ name, email, password }: any) => {
+export const signup = async ({ name, email, password, req }: any) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await createUser({ name, email, password: hashedPassword });
   sendWelcomeEmail(user.email, user.name);
@@ -14,10 +15,12 @@ export const signup = async ({ name, email, password }: any) => {
       expiresIn: '1d' 
   });
 
+  await createLog(user.id, req.ip, req.headers['user-agent']);
+
   return { user, token }; 
 };
 
-export const login = async ({ email, password }: any) => {
+export const login = async ({ email, password, req }: any) => {
   const user = await findUserByEmail(email);
   if (!user) throw new Error("User not found");
   const match = await bcrypt.compare(password, user.password);
@@ -26,6 +29,8 @@ export const login = async ({ email, password }: any) => {
   const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
     expiresIn: "1d",
   });
+
+  await createLog(user.id, req.ip, req.headers['user-agent']);
 
   return {
     user: {
