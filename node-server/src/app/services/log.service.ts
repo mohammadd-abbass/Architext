ae77399dd5bd61e329b134e34896d5903c35ce71
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-15 17:22:56 +0300

    Implemented create log function

---
+++ node-server/src/app/services/log.service.ts
@@ -0,0 +1,19 @@
+import { createLogEntry, getLogsByUserId } from '../models/log.model.js';
+import geoip from 'geoip-lite';
+
+export const createLog = async (userId: number, ipAddress: string, userAgent?: string) => {
+  const geo = (geoip.lookup(ipAddress) || {}) as geoip.Lookup;
+
+  const log = await createLogEntry({
+    userId,
+    ipAddress,
+    city: geo.city,
+    country: geo.country,
+    latitude: geo.ll?.[0],
+    longitude: geo.ll?.[1],
+    userAgent,
+  });
+
+  return log;
+};
+
