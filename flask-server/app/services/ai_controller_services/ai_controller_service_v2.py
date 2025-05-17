3e2fac599034207a18c4dce1e2416cf2e7b2f794
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 02:19:04 +0300

    Modified the generate function comments function

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -4,6 +4,11 @@ from services.langchain_client import langchain_client
 
 
 def generate_function_comments(code: str, language: str) -> str:
-    task_prompt = load_prompt("comment.md").replace("{{code}}", code)
+    raw_prompt = load_prompt("comment.md")
+    chain = langchain_client.get_text_chain(raw_prompt)
+    return chain.invoke({"language": language, "code": code})
+
+def calculate_function_complexit(code: str, language: str) -> str:
+    task_prompt = load_prompt("complexity.md").replace("{{code}}", code)
     chain = langchain_client.get_text_chain(task_prompt)
     return chain.invoke({"language": language})
