559e0b9dc7c524c08324a43c30733295103fae2d
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 02:10:01 +0300

    Implemented v2 generate function ocmments controller service function

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -1,14 +1,9 @@
-from services.langchain_client import client  
-from langchain_core.prompts import load_prompt  
+import json
+from app.utils.prompts_loader import load_prompt
+from services.langchain_client import langchain_client
+
 
 def generate_function_comments(code: str, language: str) -> str:
-
-    prompt_template = load_prompt("comment.md") 
-    
-    chain = (
-        client["base_prompt"].partial(system_prompt=prompt_template)
-        | client["llm"]
-        | client["default_parser"]
-    )
-    
-    return chain.invoke({"input": f"{language} code:\n{code}"})
+    task_prompt = load_prompt("comment.md").replace("{{code}}", code)
+    chain = langchain_client.get_text_chain(task_prompt)
+    return chain.invoke({"language": language})
