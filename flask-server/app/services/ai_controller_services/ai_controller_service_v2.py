8b76b9d85056d187c83c5fa810a2a24cdeca152f
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 02:20:12 +0300

    Implemented calculaet function complexity function

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -9,6 +9,7 @@ def generate_function_comments(code: str, language: str) -> str:
     return chain.invoke({"language": language, "code": code})
 
 def calculate_function_complexit(code: str, language: str) -> str:
-    task_prompt = load_prompt("complexity.md").replace("{{code}}", code)
-    chain = langchain_client.get_text_chain(task_prompt)
-    return chain.invoke({"language": language})
+    raw_prompt = load_prompt("complexity.md")
+    chain = langchain_client.get_text_chain(raw_prompt)
+    return chain.invoke({"language": language, "code": code})
+
