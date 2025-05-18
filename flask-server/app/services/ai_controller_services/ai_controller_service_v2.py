d321fad0213ecbf68d07101656015deac0b629b8
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 03:27:11 +0300

    Implemented analyze file against architecture controller service function

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -1,5 +1,5 @@
 import json
-from app.models.classes import ArchitectureAnalysisResult
+from app.models.classes import ArchitectureAnalysisResult, FileArchitectureResult
 from app.utils.prompts_loader import load_prompt
 from services.langchain_client import langchain_client
 
@@ -7,7 +7,7 @@ from services.langchain_client import langchain_client
 def generate_function_comments(code: str, language: str) -> str:
     raw_prompt = load_prompt("comment.md")
     chain = langchain_client.get_text_chain(raw_prompt)
-    
+
     return chain.invoke({"language": language, "code": code})
 
 
@@ -27,6 +27,20 @@ def check_project_architecture(files, reference) -> ArchitectureAnalysisResult:
 
     chain = langchain_client.get_structured_chain(
         task_prompt, ArchitectureAnalysisResult
+    )
+
+    return chain.invoke({})
+
+def analyze_file_against_architecture(code: str, language: str, reference: dict) -> FileArchitectureResult:
+    task_prompt = (
+        load_prompt("analyze.md")
+        .replace("{{code}}", code)
+        .replace("{{language}}", language)
+        .replace("{{reference}}", json.dumps(reference))
+    )
+
+    chain = langchain_client.get_structured_chain(
+        task_prompt, FileArchitectureResult
     )
 
     return chain.invoke({})
