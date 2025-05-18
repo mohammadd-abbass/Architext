839db26e5b09d4c48582d4fd15267a0216792f13
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 03:24:33 +0300

    Implemented check project architecture controller service function

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -1,4 +1,5 @@
 import json
+from app.models.classes import ArchitectureAnalysisResult
 from app.utils.prompts_loader import load_prompt
 from services.langchain_client import langchain_client
 
@@ -6,10 +7,26 @@ from services.langchain_client import langchain_client
 def generate_function_comments(code: str, language: str) -> str:
     raw_prompt = load_prompt("comment.md")
     chain = langchain_client.get_text_chain(raw_prompt)
+    
     return chain.invoke({"language": language, "code": code})
+
 
 def calculate_function_complexit(code: str, language: str) -> str:
     raw_prompt = load_prompt("complexity.md")
     chain = langchain_client.get_text_chain(raw_prompt)
+
     return chain.invoke({"language": language, "code": code})
 
+
+def check_project_architecture(files, reference) -> ArchitectureAnalysisResult:
+    task_prompt = (
+        load_prompt("refArchitecture.md")
+        .replace("{{files}}", json.dumps(files))
+        .replace("{{reference}}", json.dumps(reference))
+    )
+
+    chain = langchain_client.get_structured_chain(
+        task_prompt, ArchitectureAnalysisResult
+    )
+
+    return chain.invoke({})
