be47d82dde292bdfad496877221c4f8d48ca5440
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-18 03:50:34 +0300

    Fixed imports issues

--- flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
+++ flask-server/app/services/ai_controller_services/ai_controller_service_v2.py
@@ -1,17 +1,20 @@
 import json
-from app.models.classes import ArchitectureAnalysisResult, FileArchitectureResult
-from app.utils.prompts_loader import load_prompt
+from models.classes import ArchitectureAnalysisResult, FileArchitectureResult
+from utils.prompts_loader import load_prompt
 from services.langchain_client import langchain_client
 
 
 def generate_function_comments(code: str, language: str) -> str:
+    print("entered generate function comments")
     raw_prompt = load_prompt("comment.md")
     chain = langchain_client.get_text_chain(raw_prompt)
 
+    print(f"Code: {code}")
+    print(f"Language: {language}")
     return chain.invoke({"language": language, "code": code})
 
 
-def calculate_function_complexit(code: str, language: str) -> str:
+def calculate_function_complexity(code: str, language: str) -> str:
     raw_prompt = load_prompt("complexity.md")
     chain = langchain_client.get_text_chain(raw_prompt)
 
