ace534fd5b3fc48234d0c359e8490b47610c3c81
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-05-20 02:05:17 +0300

    Implemented create user test helper function

---
+++ node-server/src/tests/utils/testUser.helper.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { faker } from '@faker-js/faker';
+import app from '../../bootstrap/server'
+
+export async function createTestUser() {
+  const fakeUser = {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: 'password',
+  };
+
+  const response = await request(app)
+    .post('/api/auth/signup')
+    .send(fakeUser);
+
+  if (response.statusCode !== 201) {
+    throw new Error('Failed to create test user');
+  }
+
+  return fakeUser;
+}
